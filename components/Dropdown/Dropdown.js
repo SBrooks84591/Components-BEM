736f80cb5f5f39a518b0cb5df83637212974815e
sbrooks84591 <Sbrooks84591@hotmail.com>
2018-03-25 16:12:59 -0500

    Stephanie Brooks

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,6 +1,5 @@
 class Dropdown {
     constructor(element) {
-<<<<<<< HEAD
       this.element = element;
       this.button = this.element.querySelector(".Dropdown__button");
   
@@ -16,20 +15,3 @@ class Dropdown {
   
   let dropdowns = document.querySelectorAll(".Dropdown");
   dropdowns = Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
-=======
-        this.element = element;
-        this.button = this.element.querySelector(".Dropdown__button");
-
-        this.button.addEventListner("click", () => {
-            this.toggleContent();
-        });
-    }
-}
-
-toggleContent() {
-    this.element.classList.toggle("DropDown--active");
-}
-
-let dropdowns = document.querySelectorAll(".Dropdown");
-dropdowns = Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
->>>>>>> f5d742a054db865922f99644f05c77fed754a4b2
