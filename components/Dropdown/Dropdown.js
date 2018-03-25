815f7425a31bca31e823c83377aa3796cb60cefa
sbrooks84591 <Sbrooks84591@hotmail.com>
2018-03-25 04:12:56 -0500

    SB

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,17 +1,17 @@
 class Dropdown {
     constructor(element) {
-        this.element = element;
-        this.button = this.element.querySelector(".Dropdown__button");
-        
-        this.button.addEventListener("click", () => {
-            this.toggleContent();
-        });
+      this.element = element;
+      this.button = this.element.querySelector(".Dropdown__button");
+  
+      this.button.addEventListener("click", () => {
+        this.toggleContent();
+      });
     }
-
+  
     toggleContent() {
-        this.element.classList.toggle("Dropdown--active");
+      this.element.classList.toggle("Dropdown--active");
     }
-}
-
-let dropdowns = document.querySelectorAll(".Dropdown");
-drowdowns = Array.from(dropdown).map(dropdown => new Dropdown(dropdown));
+  }
+  
+  let dropdowns = document.querySelectorAll(".Dropdown");
+  dropdowns = Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
